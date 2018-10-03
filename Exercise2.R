9696a3d23b8cb3b43834a3e4cc294f71f621ff12
Drew836 <hill735@outlook.com>
2018-10-03 15:48:35 -0700

    Changed read.csv to load boat variable as character instead of factor.

--- Exercise2.R
+++ Exercise2.R
@@ -2,5 +2,33 @@ library(dplyr)
 library(tidyr)
 
 
-data <- read.csv("titanic_original.csv")
+data <- read.csv("titanic_original.csv", as.is = c("boat"))
 df <- tbl_df(data)
+
+#
+# Change blank embarked variable to S
+#
+df[df$embarked == "","embarked"] <- "S"
+
+#
+#Change NA age variable to mean of age
+#
+
+mean.age <- round(mean(df$age, na.rm = TRUE), digits = 0)
+df[is.na(df$age), "age"] <- mean.age
+
+#
+#Change lifeboat variable to None for missing values
+#
+df[df$boat == "", "boat"] <- "None"
+
+#
+# Add variable has_cabin_number to dataset based upon cabin variable
+#
+df2 <- mutate(df, has_cabin_number = ifelse(cabin != "", 1, 0))
+
+
+#
+# Output cleaned .csv file
+#
+write.csv(df2, "titanic_clean.csv")
